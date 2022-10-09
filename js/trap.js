a6d681f1008c395c6088e022b49099e2dcb44950
marcus0utcast <115426051+marcus0utcast@users.noreply.github.com>
2022-10-09 23:38:50 +0300

    Add files via upload

---
+++ js/trap.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".button_main");
+const image = document.querySelector(".img");
+const url = "https://api.waifu.pics/nsfw/trap";
+
+async function fetchHandler() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        image.src = data.url;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+button,addEventListener("click", () => {
+    fetchHandler();
+})
